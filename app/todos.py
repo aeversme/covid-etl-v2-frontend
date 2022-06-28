0691db4eefcd02c1f63cd501c7adbfd20274ec19
Alex <aeversme@gmail.com>
2022-06-27 22:12:54 -0400

    add todos

---
+++ app/todos.py
@@ -0,0 +1,7 @@
+# TODO: project covid data onto map - start with state data, then county data
+
+# TODO: tooltip and legend for map
+
+# TODO: have metrics reflect state data when a state is selected
+
+# TODO: graph of historical data below map
